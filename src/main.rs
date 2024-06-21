b34d3281305f727d4104992ed86a6f4909330bfe
Quaternions <krakow20@gmail.com>
2024-06-20 19:54:55 -0700

    use fewer arbitrary precision calculations

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ fn main() {
 		let pi=rug::Float::with_val(precision,rug::float::Constant::Pi);
 		let big_n=rug::Float::with_val(precision,n);
 		let val=(pi*big_n.sqrt()).exp();
-		let int=val.clone().round();
-		let diff_log=(val-int).abs().log10().to_f32();
-		if diff_log< -5.0{
-			println!("notable: {n} digits:{diff_log}");
+		let fract=val.fract().to_f64();
+		let dist_log=fract.min(1.0-fract).log10();
+		if dist_log < -5.0{
+			println!("notable: {n}, distance (log10): {dist_log:.2}");
 		}
 	}
 }
